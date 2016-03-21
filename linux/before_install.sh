49527cd3521bcc5cd36df44eeb0568c1824e2d8b
malublu <admin@malublu.de>
2016-03-21 20:53:09 +0100

    console overflow

--- linux/before_install.sh
+++ linux/before_install.sh
@@ -4,8 +4,8 @@ mkdir dependencies
 mkdir build
 cd dependencies
 wget http://download.qt.io/official_releases/qt/5.6/5.6.0/single/qt-everywhere-opensource-src-5.6.0.tar.gz
-gunzip qt-everywhere-opensource-src-5.6.0.tar.gz
-tar xvf qt-everywhere-opensource-src-5.6.0.tar
+gunzip -qq qt-everywhere-opensource-src-5.6.0.tar.gz
+tar -qq xvf qt-everywhere-opensource-src-5.6.0.tar
 
 export CXX=g++
 export CC=gcc
