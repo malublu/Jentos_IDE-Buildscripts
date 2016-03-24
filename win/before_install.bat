a288e87a7bb2a8e06b0bf7e9701787f814a06fb0
malublu <admin@malublu.de>
2016-03-24 19:57:47 +0100

    fix jom install

--- win/before_install.bat
+++ win/before_install.bat
@@ -3,8 +3,7 @@ set MINGWDIR=C:\Qt\Tools\mingw482_32
 set QTDIR=C:\Qt\5.3\msvc2013_opengl
 set QTBINDIR=%QTDIR%\bin
 set PATH=%PATH%;%MINGWDIR%;%QTBINDIR%
-mkdir dependencies
-mkdir build
+md dependencies
 cd dependencies
 curl -o jom.zip http://download.qt.io/official_releases/jom/jom.zip
 cd %builddir%
--- win/install.bat
+++ win/install.bat
@@ -1,4 +1,5 @@
 cd dependencies
+md C:\jom\
 copy jom.zip C:\jom\jom.zip
 7z.exe e C:\jom\jom.zip
 rm -f C:\jom\jom.zip
