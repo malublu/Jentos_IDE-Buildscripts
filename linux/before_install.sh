618bb52f2a4756f3fe28773c18af165e3a236347
malublu <admin@malublu.de>
2016-03-24 23:05:04 +0100

    cleanup osx

--- linux/before_install.sh
+++ linux/before_install.sh
@@ -2,10 +2,7 @@
 builddir="$PWD"
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-mkdir dependencies
 mkdir build
-cd dependencies
-curl -L -O http://download.qt.io/official_releases/qt/5.3/5.3.2/single/qt-everywhere-opensource-src-5.3.2.zip
 
 export CXX=g++
 export CC=gcc
--- osx/install.sh
+++ osx/install.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 # --- QT --- #
-cd dependencies
-unzip -qq qt-everywhere-opensource-src-5.3.2.zip
-cd qt-everywhere-opensource-src-5.3.2
-chmod +x configure
-./configure -release -opensource -confirm-license -fast -system-zlib -qt-libtiff -qt-libpng -qt-libjpeg -nomake demos -nomake examples -platform macx-clang-32
-make
-make install
-export PATH=/usr/local/Qt-5.3.2/bin:$PATH
-cd ..
-cd ..
+brew install qt53
+brew link --force qt53
 # --- Additional --- #
 brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
 brew link --force sshpass
