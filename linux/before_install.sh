c7cbac0d7c141b72c92561ef2709a22ca4a361ae
malublu <admin@malublu.de>
2016-03-24 22:54:57 +0100

    cleanup linux

--- linux/before_install.sh
+++ linux/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 builddir="$PWD"
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
 mkdir dependencies
 mkdir build
--- linux/install.sh
+++ linux/install.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 # --- QT --- #
-sudo apt-get install -y build-essential perl python git
-sudo apt-get install -y build-dep qt5-default
-sudo apt-get install -y "^libxcb.*" libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
-sudo apt-get install -y libssl-dev libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libfontconfig1-dev
-sudo apt-get install -y libcap-dev libbz2-dev libgcrypt11-dev libpci-dev libnss3-dev build-essential libxcursor-dev libxcomposite-dev libxdamage-dev libxrandr-dev libdrm-dev libfontconfig1-dev libxtst-dev libasound2-dev gperf libcups2-dev libpulse-dev libudev-dev libssl-dev flex bison ruby libxss-dev libatkmm-1.6-dev
-sudo apt-get install -y libasound2-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-sudo apt-get install -y libglu1-mesa-dev
-sudo apt-get install unzip
-cd dependencies
-unzip -qq qt-everywhere-opensource-src-5.3.2.zip
-cd qt-everywhere-opensource-src-5.3.2
-chmod +x configure
-./configure -release -opensource -confirm-license -fast -system-zlib -qt-libtiff -qt-libpng -qt-libjpeg -nomake demos -nomake examples -platform linux-g++-32
-make
-make install
-export PATH=/usr/local/Qt-5.3.2/bin:$PATH
-cd ..
-cd ..
+sudo apt-get install qt5-default libqt5webkit5-dev qtdeclarative5-dev sshpass zip
 # --- Additional --- #
 cd /usr/local
 sudo mkdir linuxdeployqt
