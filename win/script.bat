bc3914447f1b72c704115a3cf08e7a285e4e6891
malublu <admin@malublu.de>
2016-03-24 21:00:34 +0100

    full path for jom

--- win/script.bat
+++ win/script.bat
@@ -1,6 +1,6 @@
 qmake jentos_ide.pro -r -spec win32-msvc2013
-jom
-jom clean
+C:\jom\jom.exe
+C:\jom\jom.exe clean
 windeployqt --release .\release\JentosIDE.exe
 copy %QTBINDIR%\libwinpthread-1.dll .\release /y
 copy "%QTBINDIR%\libstdc++-6.dll" .\release /y
