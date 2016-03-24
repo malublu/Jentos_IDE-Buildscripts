fc0bb3e66e4d352cc61627559c44cf3057b3c2af
malublu <admin@malublu.de>
2016-03-24 18:35:49 +0100

    fixing command not found

--- linux/script.sh
+++ linux/script.sh
@@ -2,7 +2,7 @@ cd build
 qmake -makefile -o Makefile ../jentos_ide.pro -r -spec linux-g++-32
 make
 make clean
-linuxdeployqt JentosIDE
+/usr/local/linuxdeployqt/linuxdeployqt JentosIDE
 cd ..
 zip -r $TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BRANCH.zip ./build
 cd "$builddir"
