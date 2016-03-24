b3721a3251cfde720cb764089c0585c998019e33
malublu <admin@malublu.de>
2016-03-24 23:16:22 +0100

    use qt55

--- osx/install.sh
+++ osx/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # --- QT --- #
-brew install qt53
-brew link --force qt53
+brew install qt55
+brew link --force qt55
 # --- Additional --- #
 brew install https://raw.githubusercontent.com/kadwanev/bigboybrew/master/Library/Formula/sshpass.rb
 brew link --force sshpass
