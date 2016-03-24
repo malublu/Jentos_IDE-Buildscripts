e1dcb0afc1a3d6ba4117701e528923b7322dc0fa
malublu <admin@malublu.de>
2016-03-24 21:20:51 +0100

    fix unzip of jom

--- win/install.bat
+++ win/install.bat
@@ -1,8 +1,6 @@
 cd dependencies
 md C:\jom\
-copy jom.zip C:\jom\jom.zip
-7z.exe e C:\jom\jom.zip
-rm -f C:\jom\jom.zip
+7z.exe e jom.zip -o C:\jom
 set JOM=C:\jom
 set PATH=%PATH%;%JOM%
 cd %builddir%
