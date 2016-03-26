773f559bc457a871f2c5999b39c7fe91a2381234
malublu <admin@malublu.de>
2016-03-26 17:11:28 +0100

    enable cl build

--- win/before_install.bat
+++ win/before_install.bat
@@ -3,6 +3,7 @@ set MINGWDIR=C:\Qt\Tools\mingw482_32
 set QTDIR=C:\Qt\5.3\msvc2013_opengl
 set QTBINDIR=%QTDIR%\bin
 set PATH=%PATH%;%MINGWDIR%;%QTBINDIR%
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\vcvars32.bat"
 md dependencies
 cd dependencies
 curl -L -o jom.zip http://download.qt.io/official_releases/jom/jom.zip
