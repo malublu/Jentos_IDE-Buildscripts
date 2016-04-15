a128e7e3dec0046059f27a2d9a75414a452b02ce
malublu <admin@malublu.de>
2016-04-15 14:50:20 +0200

    Fix for: The following packages have unmet dependencies

--- linux/install.sh
+++ linux/install.sh
@@ -2,6 +2,7 @@
 # --- QT --- #
 sudo apt-get install qt5-default:i386 libqt5webkit5-dev:i386 qtdeclarative5-dev:i386 sshpass zip
 sudo apt-get install gcc-multilib g++-multilib
+sudo apt-get install -f
 # --- Additional --- #
 cd /usr/local
 sudo mkdir linuxdeployqt
