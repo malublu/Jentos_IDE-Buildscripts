42445909ed9a5a40ef9565ade5b3b51a1bce92f1
malublu <admin@malublu.de>
2016-04-15 14:56:26 +0200

    i386 not working

--- linux/install.sh
+++ linux/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # --- QT --- #
-sudo apt-get install qt5-default:i386 libqt5webkit5-dev:i386 qtdeclarative5-dev:i386 sshpass zip
+sudo apt-get install qt5-default libqt5webkit5-dev qtdeclarative5-dev sshpass zip
 sudo apt-get install gcc-multilib g++-multilib
 sudo apt-get install -f
 # --- Additional --- #
