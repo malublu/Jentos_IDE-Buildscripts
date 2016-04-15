51599b78c7e2e74bf15e2d899070b4c6782d37cb
malublu <admin@malublu.de>
2016-04-15 15:16:46 +0200

    remove upgrade prompt

--- linux/before_install.sh
+++ linux/before_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 builddir="$PWD"
-sudo dpkg --add-architecture i386
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo apt-get update -qq
-sudo apt-get -y upgrade -qq
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y upgrade -qq
 mkdir build
 
 export CXX=g++
