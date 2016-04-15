3791b1616923af462d7a2b5d5baa753745ecbbfe
malublu <admin@malublu.de>
2016-04-15 16:46:23 +0200

    fix windows build

--- linux/script.sh
+++ linux/script.sh
@@ -3,7 +3,7 @@ cd build
 qmake -makefile -o Makefile ../jentos_ide.pro -r -spec linux-g++
 make
 make clean
-/usr/local/linuxdeployqt/linuxdeployqt JentosIDE
+/usr/local/linuxdeployqt/linuxdeployqt ./JentosIDE
 cd ..
 zip -r $TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BRANCH.zip ./build
 cd "$builddir"
