c1e50abdb3c199c112453c53e69b8c281de4ca27
malublu <admin@malublu.de>
2016-04-15 16:56:04 +0200

    i don't know why it not work

--- win/before_install.bat
+++ win/before_install.bat
@@ -1,9 +1,12 @@
 set "builddir=%cd%"
+
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
+
 set MINGWDIR=C:\Qt\Tools\mingw482_32
 set QTDIR=C:\Qt\5.3\msvc2013_opengl
 set QTBINDIR=%QTDIR%\bin
 set PATH=%PATH%;%MINGWDIR%;%QTBINDIR%
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+
 md dependencies
 cd dependencies
 curl -L -o jom.zip http://download.qt.io/official_releases/jom/jom.zip
