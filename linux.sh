472f0d1012b06cd1e3bbbad420320c1885ce20e8
malublu <admin@malublu.de>
2016-05-20 09:03:15 +0200

    set chmod in download script

--- linux.sh
+++ linux.sh
@@ -1,3 +1,6 @@
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/linux/before_install.sh
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/linux/install.sh
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/linux/script.sh
+chmod +x before_install.sh
+chmod +x install.sh
+chmod +x script.sh
--- osx.sh
+++ osx.sh
@@ -1,3 +1,6 @@
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/osx/before_install.sh
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/osx/install.sh
 curl -fsSL -O https://raw.githubusercontent.com/malublu/Jentos_IDE-Buildscripts/master/osx/script.sh
+chmod +x before_install.sh
+chmod +x install.sh
+chmod +x script.sh
