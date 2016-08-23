4da70ac8cd3e51a618fb43d47998cc1a0f254e57
malublu <admin@malublu.de>
2016-08-23 14:14:59 +0200

    Fix macdeployqt

--- osx/script.sh
+++ osx/script.sh
@@ -2,7 +2,9 @@ cd build
 qmake -makefile -o Makefile ../jentos.code.pro -r -platform macx-clang
 make
 make clean
-macdeployqt ./build/exec/Jentos.Code.app -dmg
+cd ./build/exec/
+macdeployqt Jentos.Code.app -dmg
+cd ../../
 cd ..
 zip -r $TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BRANCH.zip ./build
 cd "$builddir"
