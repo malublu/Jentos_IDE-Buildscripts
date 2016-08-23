df5bad5c2fd37954843c28ade981630e4eeae07f
malublu <admin@malublu.de>
2016-08-23 15:30:57 +0200

    change app dir

--- osx/script.sh
+++ osx/script.sh
@@ -2,9 +2,7 @@ cd build
 qmake -makefile -o Makefile ../jentos.code.pro -r -platform macx-clang
 make
 make clean
-cd ./build/exec/
-macdeployqt Jentos.Code.app -dmg
-cd ../../
+macdeployqt ./exec/Jentos.Code.app -dmg
 cd ..
 zip -r $TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BRANCH.zip ./build
 cd "$builddir"
