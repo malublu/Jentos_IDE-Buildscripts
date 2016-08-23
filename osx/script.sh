6d65c7fc9b3b36406809c4f2b8562ab2825379a7
malublu <Luka Bluhm@PC1931.fwh-eg.gmh-group.local>
2016-08-23 13:46:30 +0200

    add .app and use dmg

--- osx/script.sh
+++ osx/script.sh
@@ -2,7 +2,7 @@ cd build
 qmake -makefile -o Makefile ../jentos.code.pro -r -platform macx-clang
 make
 make clean
-macdeployqt ./build/exec/Jentos.Code
+macdeployqt ./build/exec/Jentos.Code.app -dmg
 cd ..
 zip -r $TRAVIS_OS_NAME-$TRAVIS_COMMIT-$TRAVIS_BRANCH.zip ./build
 cd "$builddir"
